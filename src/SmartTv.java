944470fcddace5886c84f7c6a27c07d4a9d57fd5
Alexandre <xandirsilva64@gmail.com>
2022-10-25 17:05:45 -0300

    SmartTV Commit!

--- src/SmartTv.java
+++ src/SmartTv.java
@@ -3,7 +3,24 @@ public class SmartTv {
     int canal=7;
     int volume=24;
 
+     public void aumentarVolume(){
+         //volume = volume +1;
+         volume++;
+     }
+
+    public void diminuirVolume() {
+        //volume = volume -1;
+        volume--;
+    }
 
 
 
-}
+    public void ligar() {
+            ligada = true;
+        }
+            public void desligar(){
+                ligada=false;
+
+
+            }
+        }
